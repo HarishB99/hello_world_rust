a2cc727b72b241bed6546eae44986e6aac9be210
HarishB99 <harish_balamurugan@yahoo.com.sg>
2024-10-16 12:35:56 +0800

    init: hello world rust

---
+++ hello_world_rust.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("An unusual way to create a hello world project.");
+    println!("Cargo.toml was manually created.");
+    println!("The source file was manually configured in Cargo.toml.");
+    println!("`cargo new/cargo init` was not used.");
+    println!("Instead of creating main.rs, hello_world_rust.rs was created. (which is me by the way)");
+}
